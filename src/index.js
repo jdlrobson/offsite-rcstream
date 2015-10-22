f7fa2be035a504460cc461ffea236f791734601c
jdlrobson <jdlrobson@gmail.com>
2015-10-22 10:29:03 +0200

    Add a edit per second stream
    Changes:
    * Now hits all our wikis not just commons

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import './index.less'
 import App from './ui/app.js'
 
 import stream from './net/rc.js'
+import editsPerSecondStream from './streams/edits-per-second.js'
 
 function render (edits) {
   ReactDOM.render(<App edits={edits}/>, document.body)
 }
 
 var edits = []
-stream.on('message', (m) => { console.log(m); edits.push(m); render(edits); })
-render(edits)
+var eps = editsPerSecondStream( stream );
+
+stream.on('message', (m) => { edits.push(m); render(edits); })
+eps.on('message', (m) => { console.log('yo',m) })
